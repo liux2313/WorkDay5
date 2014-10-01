eb980bd2ab015bc3c1afd3939336d2cbf6aa28b9
idsc3101 <idsc3101@gmail.com>
2014-10-01 15:26:27 -0500

    changes made

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -9,7 +9,14 @@ Public Class Form1
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        Dim idxSpace As Integer
+
+        idxSpace = name.IndexOf(" ")
+
+        Dim fstname As String
+        fstname = name.Substring(0, idxSpace)
+
+        Return fstname
     End Function
 
 End Class
